fb69357245d0b98369c1f6a8870d22af89a02f68
Drymander <johnnydryman@gmail.com>
2021-07-14 15:19:26 -0400

    7/14 update

--- first_app.py
+++ first_app.py
@@ -1,13 +1,118 @@
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import seaborn as sns
+import numpy as np
+
+#Standard python libraries
+import pandas as pd
+import seaborn as sns
+# sns.set_context('talk')
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+warnings.filterwarnings(action='ignore') 
+
+# Preprocessing tools
+from sklearn.model_selection import train_test_split,cross_val_predict,cross_validate
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,OneHotEncoder
+scaler = StandardScaler()
+from sklearn import metrics
+
+# Models & Utilities
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LogisticRegression,LogisticRegressionCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, precision_score, recall_score, f1_score
+
+# Warnings
+import warnings
+warnings.filterwarnings(action='ignore') 
+
+# NLP Libraries
+import nltk
+import collections
+# nltk.download('punkt')
+from sklearn.manifold import TSNE
+from nltk.tokenize import word_tokenize
+from nltk import regexp_tokenize
+import re
+from nltk.corpus import stopwords
+from nltk.collocations import *
+from nltk import FreqDist
+from nltk import word_tokenize
+from nltk import ngrams
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+nltk.download('stopwords')
+# !pip install wordcloud
+from wordcloud import WordCloud
+
+import json
+def get_keys(path):
+    with open(path) as f:
+        return json.load(f)
+# keys = get_keys("/Users/Johnny/.secret/yelp_api.json")
+# api_key = keys['api_key']
+# pip install tmdbsimple #Ctrl+? this line to install tmdbsimple
+import tmdbsimple as tmdb
+# tmdb.API_KEY = api_key
+
+scrape = False
+
+import ast
+import time
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+api_key = 'ceeaacb7cf024c7485e00ef8457e42dc'
+
+
+# user = 'Drymander'
+
+import pickle
+from tqdm import tqdm
+
+# pip install isodate
+
+import isodate
+
+
 import streamlit as st
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
 import numpy as np
 import pandas as pd
 
-st.title('My first fdsfdsa app')
+st.title('Single Match Data')
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+
+
+df = pd.read_csv('match.csv')
+st.dataframe(df)
+
+fig = plt.figure()
+fig.patch.set_facecolor('white')
+fig.patch.set_alpha(0.6)
+
+ax = plt.axes()
+ax.set_facecolor("white")
+# OR
+
+plt.barh( df['Gamertag'], df['TotalTimePlayed'].sort_values())
+
+# setting label of y-axis
+plt.ylabel("Gamertag")
+  
+# setting label of x-axis
+plt.xlabel("Hours") 
+plt.title("Time Played")
+plt.show()
